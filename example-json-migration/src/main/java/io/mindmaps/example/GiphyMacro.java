0dd661e83508045fa37fbb37959cdb03f8801d60
Alexandra Orth <alexandra@mindmaps.io>
2016-11-10 15:57:30 +0000

    SQL migration example

---
+++ example-json-migration/src/main/java/io/mindmaps/example/GiphyMacro.java
@@ -0,0 +1,46 @@
+/*
+ * MindmapsDB - A Distributed Semantic Database
+ * Copyright (C) 2016  Mindmaps Research Ltd
+ *
+ * MindmapsDB is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MindmapsDB is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MindmapsDB. If not, see <http://www.gnu.org/licenses/gpl.txt>.
+ */
+package io.mindmaps.example;
+
+import io.mindmaps.graql.internal.template.Value;
+import io.mindmaps.graql.internal.template.macro.Macro;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Macro that will take the value from the "slug" field, split it by -
+ * and return all but the last value of the split.
+ */
+public class GiphyMacro implements Macro<List<String>> {
+
+    public List<String> apply(List<Value> values) {
+        // get first value as string
+        String slug = values.get(0).asString();
+
+        // split
+        String[] keywords = slug.split("-");
+
+        // return all except last
+        return Arrays.asList(Arrays.copyOf(keywords, keywords.length-1));
+    }
+
+    public String name() {
+        return "giphy";
+    }
+}
---
+++ example-json-migration/src/main/java/io/mindmaps/example/Main.java
@@ -0,0 +1,86 @@
+/*
+ * MindmapsDB - A Distributed Semantic Database
+ * Copyright (C) 2016  Mindmaps Research Ltd
+ *
+ * MindmapsDB is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MindmapsDB is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MindmapsDB. If not, see <http://www.gnu.org/licenses/gpl.txt>.
+ */
+package io.mindmaps.example;
+
+import io.mindmaps.engine.loader.BlockingLoader;
+import io.mindmaps.engine.loader.Loader;
+import io.mindmaps.graql.Graql;
+import io.mindmaps.migration.base.Migrator;
+import io.mindmaps.migration.base.io.MigrationLoader;
+import io.mindmaps.migration.json.JsonMigrator;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static java.util.stream.Collectors.joining;
+
+public class Main {
+
+    private static final String GRAPH_NAME = "giphy";
+    private static final String DATA_DIR = "trending";
+    private static final String TEMPLATE = "template.gql";
+    private static final String ONTOLOGY = "ontology.gql";
+
+    private static final Loader loader = new BlockingLoader(GRAPH_NAME);
+
+    public static void main(String[] args){
+
+        try {
+            // load your ontology
+            loadOntology();
+
+            // get resources
+            String template = getResourceAsString(TEMPLATE);
+            File jsonData = new File(getResource(DATA_DIR).toString());
+
+            // create a migrator with your macro
+            Migrator migrator = new JsonMigrator(template, jsonData)
+                    .registerMacro(new GiphyMacro());
+
+            System.out.println("Beginning migration");
+
+            // load data in directory
+            MigrationLoader.load(loader, migrator);
+
+            System.out.println("Migration complete");
+        } catch (IOException e){
+            throw new RuntimeException(e);
+        }
+
+        System.exit(0);
+    }
+
+    public static void loadOntology() throws IOException {
+        String ontology = getResourceAsString(ONTOLOGY);
+
+        loader.add(Graql.parse(ontology));
+        loader.flush();
+        loader.waitToFinish();
+    }
+
+    public static Path getResource(String resourceName){
+        return Paths.get(Main.class.getClassLoader().getResource(resourceName).getPath());
+    }
+
+    public static String getResourceAsString(String resourceName) throws IOException {
+        return Files.readAllLines(getResource(resourceName)).stream().collect(joining("\n"));
+    }
+}
