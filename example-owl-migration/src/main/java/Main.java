1020b518d481f8432852994fdf6b5be605513e8a
Alexandra Orth <alexandra@mindmaps.io>
2016-11-11 11:11:51 +0000

    Owl updated to grakn

--- example-owl-migration/src/main/java/Main.java
+++ example-owl-migration/src/main/java/Main.java
@@ -16,65 +16,28 @@
  * along with MindmapsDB. If not, see <http://www.gnu.org/licenses/gpl.txt>.
  */
 
-import ch.qos.logback.classic.Level;
-import io.mindmaps.Mindmaps;
-import io.mindmaps.MindmapsGraph;
-import io.mindmaps.util.REST;
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLConnection;
-import ch.qos.logback.classic.Logger;
+import ai.grakn.Grakn;
+import ai.grakn.GraknGraph;
+import ai.grakn.engine.GraknEngineServer;
 
 public class Main {
     private static final String keyspace = "FAMILY";
     private static String filePath = "family.owl";
 
     public static void main(String[] args) {
-        disableInternalLogs();
-
-        if(!mindmapsEngineRunning()){
-            System.out.println("Please start Mindmaps Engine");
-            System.out.println("You can get more information on how to do so using our setup guide: https://mindmaps.io/pages/documentation/get-started/setup-guide.html");
+        if(!GraknEngineServer.isRunning()){
+            System.out.println("Please start Grakn Engine");
+            System.out.println("You can get more information on how to do so using our setup guide: https://grakn.ai/pages/documentation/get-started/setup-guide.html");
             return;
         } else {
             System.out.println("=================================================================================================");
-            System.out.println("|||||||||||||||||||||||||||||||   Mindmaps OWL Migration Example   ||||||||||||||||||||||||||||||");
+            System.out.println("|||||||||||||||||||||||||||||||||   Grakn OWL Migration Example   ||||||||||||||||||||||||||||||||");
             System.out.println("=================================================================================================");
         }
         // get connection to the graph
-        MindmapsGraph graph = Mindmaps.factory(Mindmaps.DEFAULT_URI, keyspace).getGraph();
+        GraknGraph graph = Grakn.factory(Grakn.DEFAULT_URI, keyspace).getGraph();
 
         OWLResourceMigrator.migrate(filePath, graph);
         OWLResourceMigrator.printInformationAboutWorld(graph);
-    }
-
-    /**
-     * Check if Mindmaps Engine is running
-     * @return true if mindmaps engine is running
-     */
-    public static boolean mindmapsEngineRunning(){
-        try {
-            URL url = new URL("http://localhost:4567" + REST.WebPath.GRAPH_FACTORY_URI);
-            URLConnection conn = url.openConnection();
-            HttpURLConnection http = (HttpURLConnection) conn;
-            http.setRequestMethod("GET");
-            http.setDoOutput(true);
-            http.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
-
-            http.getResponseMessage();
-        }
-        catch (IOException e){
-            return false;
-        }
-        return true;
-    }
-
-    /**
-     * Disables extra logs which come from cassandra when using the TitanFactory
-     */
-    private static void disableInternalLogs(){
-        Logger logger = (Logger) org.slf4j.LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
-        logger.setLevel(Level.OFF);
     }
 }
--- example-owl-migration/src/main/java/OWLResourceMigrator.java
+++ example-owl-migration/src/main/java/OWLResourceMigrator.java
@@ -1,41 +1,40 @@
 /*
- * MindmapsDB - A Distributed Semantic Database
- * Copyright (C) 2016  Mindmaps Research Ltd
+ * GraknDB - A Distributed Semantic Database
+ * Copyright (C) 2016  Grakn Research Ltd
  *
- * MindmapsDB is free software: you can redistribute it and/or modify
+ * GraknDB is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * MindmapsDB is distributed in the hope that it will be useful,
+ * GraknDB is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with MindmapsDB. If not, see <http://www.gnu.org/licenses/gpl.txt>.
+ * along with GraknDB. If not, see <http://www.gnu.org/licenses/gpl.txt>.
  */
 
-import io.mindmaps.MindmapsGraph;
-import io.mindmaps.graql.Graql;
-import io.mindmaps.graql.MatchQuery;
-import io.mindmaps.graql.QueryBuilder;
-import io.mindmaps.graql.Reasoner;
-import io.mindmaps.migration.owl.OWLMigrator;
+import ai.grakn.GraknGraph;
+import ai.grakn.graql.MatchQuery;
+import ai.grakn.graql.QueryBuilder;
+import ai.grakn.graql.Reasoner;
+import ai.grakn.migration.owl.OWLMigrator;
 import java.io.InputStream;
 import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
 
-import static io.mindmaps.graql.Graql.count;
-import static io.mindmaps.graql.Graql.var;
+import static ai.grakn.graql.Graql.count;
+import static ai.grakn.graql.Graql.var;
 
 
 public class OWLResourceMigrator {
 
-    public static void migrate(String resource, MindmapsGraph graph){
+    public static void migrate(String resource, GraknGraph graph){
         OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
-        OWLMigrator migrator = new io.mindmaps.migration.owl.OWLMigrator();
+        OWLMigrator migrator = new ai.grakn.migration.owl.OWLMigrator();
         OWLOntology ontology;
 
         try (InputStream in = OWLResourceMigrator.class.getResourceAsStream(resource)) {
@@ -53,7 +52,7 @@ public class OWLResourceMigrator {
     /**
      * Prints information about the migrated database
      */
-    public static void printInformationAboutWorld(MindmapsGraph graph){
+    public static void printInformationAboutWorld(GraknGraph graph){
         graph.rollback();
 
         // What are the types that were migrated?
