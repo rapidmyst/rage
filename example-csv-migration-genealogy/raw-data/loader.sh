45dcf5ebf24a53fbfa098082e237fec2afd3f6a8
Jo Stichbury <jo@mindmaps.io>
2017-03-03 12:42:40 +0000

    Update CSV migration example to use keyspace as it is now required

--- example-csv-migration-genealogy/raw-data/loader.sh
+++ example-csv-migration-genealogy/raw-data/loader.sh
@@ -2,14 +2,14 @@
 
 if [ -n "$1" ]
 then
-	echo "Importing Ontology"
+	echo "Importing Ontology into default keyspace"
 	$1/graql.sh -f $PWD/../ontology.gql
-	echo "Migrating people"
-	$1/migration.sh csv -i $PWD/people.csv -t $PWD/migrators/people-migrator.gql
-	echo "Migrating births"
-	$1/migration.sh csv -i $PWD/births.csv -t $PWD/migrators/births-migrator.gql
-	echo "Migrating weddings"
-	$1/migration.sh csv -i $PWD/weddings.csv -t $PWD/migrators/weddings-migrator.gql
+	echo "Migrating people into default keyspace"
+	$1/migration.sh csv -i $PWD/people.csv -t $PWD/migrators/people-migrator.gql -k grakn
+	echo "Migrating births into default keyspace"
+	$1/migration.sh csv -i $PWD/births.csv -t $PWD/migrators/births-migrator.gql -k grakn
+	echo "Migrating weddings into default keyspace"
+	$1/migration.sh csv -i $PWD/weddings.csv -t $PWD/migrators/weddings-migrator.gql -k grakn
 	echo "Done migrating data"
 
 else
